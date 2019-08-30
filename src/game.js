12b6e358c5a4622f61705c7a373cf880f2bd6457
codrus <noreply@codrus.com>
2019-08-30 11:22:07 -0400

    Keyboard control and framerate counter added

--- src/game.js
+++ src/game.js
@@ -1,71 +1,72 @@
-﻿var character;
+﻿"use strict";
+
+var character;
 var animated;
 
 var Game = {
 	canvas: 0,
-	frame_counter: 1,
+	keys: [],
+	frames: 0,
+	seconds: 0,
 	start: function (page_canvas, frame_time) {
 		// Get the graphics context
 		this.canvas = page_canvas;
 		this.context = this.canvas.getContext("2d");
 
+		// Catch key presses
+		window.addEventListener("keydown", this.keyPress);
+		window.addEventListener("keyup", this.keyRelease);
+
 		// Trigger canvas updates
 		this.interval = setInterval(this.update, frame_time);
+		this.intervalb = setInterval(this.framerate, 1000);
+
+		this.log_status = document.getElementById("status");
 
 		/// Testing stuff ///
 		character = new Sprite(100, 100, 16, 16, 16, 16, 1, "img/guy.png");
 		animated = new Sprite(50, 50, 16, 16, 128, 64, 1, "img/testanim.png");
 	},
-	clear: function () {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	},
 	stop: function () {
 		clearInterval(this.interval);
 	},
+	clear: function () {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	},
 	update: function () {
+		// Clear the canvas
 		Game.clear();
-		character.update();
-		animated.update();
 
-		Game.frame_counter++;
-		var status = document.getElementById("status");
-		status.innerHTML = Game.frame_counter;
-	}
-};
-
-function Sprite(x, y, sprite_width, sprite_height, sheet_width, sheet_height, speed, file) {
-	// Set the image
-	this.image = new Image();
-	this.image.src = file;
-
-	// Set the size and location
-	this.x = x;
-	this.y = y;
-	this.width = sprite_width;
-	this.height = sprite_height;
-
-	this.sheet_width = sheet_width;
-	this.sheet_height = sheet_height;
-
-	this.subimage = 0;
-	this.skip = 0;
-	this.speed = speed;
-
-	// Set the update callback
-	this.update = function () {
-		// Draw a sprite
-		Game.context.drawImage(this.image, this.subimage * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
-
-		// Increment the animation
-		this.skip++;
-		if (this.skip >= speed) {
-			this.skip = 0;
-
-			this.subimage++;
-			if (this.subimage * this.width >= this.sheet_width) {
-				this.subimage = 0;
-			}
+		if (Game.keys[65]) {
+			character.x -= 1.5;
 		}
+		if (Game.keys[68]) {
+			character.x += 1.5;
+		}
+		if (Game.keys[87]) {
+			character.y -= 1.5;
+		}
+		if (Game.keys[83]) {
+			character.y += 1.5;
+		}
+
+		// Draw stuff
+		character.draw();
+		animated.animate();
+
+		Game.frames++;
+	},
+	keyPress: function (e) {
+		Game.keys[e.keyCode] = true;
+	},
+	keyRelease: function (e) {
+		Game.keys[e.keyCode] = false;
+	},
+
+	framerate: function () {
+		Game.seconds++;
+		Game.log_status.innerHTML = "TIME " + Game.seconds + "<br>FPS " + Game.frames;
+		Game.frames = 0;
 	}
 };
 
@@ -76,5 +77,5 @@ function beginGame() {
 	canvas.height = 240;
 
 	// Start the game
-	Game.start(canvas, 33);
+	Game.start(canvas, 5);
 };
---
+++ src/sprite.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+function Sprite(x, y, sprite_width, sprite_height, sheet_width, sheet_height, speed, file) {
+	// Set the image
+	this.image = new Image();
+	this.image.src = file;
+
+	// Set the size and location
+	this.x = x;
+	this.y = y;
+	this.width = sprite_width;
+	this.height = sprite_height;
+
+	this.sheet_width = sheet_width;
+	this.sheet_height = sheet_height;
+
+	this.subimage = 0;
+	this.skip = 0;
+	this.speed = speed;
+
+	// Set the update callback
+	this.draw = function () {
+		// Draw the whole sprite
+		Game.context.drawImage(this.image, Math.round(this.x), Math.round(this.y), this.width, this.height);
+	}
+	this.animate = function () {
+		// Draw a subimage from the sprite
+		Game.context.drawImage(this.image, this.subimage * this.width, 0, this.width, this.height, Math.round(this.x), Math.round(this.y), this.width, this.height);
+
+		// Increment the animation
+		this.skip++;
+		if (this.skip >= speed) {
+			this.skip = 0;
+
+			this.subimage++;
+			if (this.subimage * this.width >= this.sheet_width) {
+				this.subimage = 0;
+			}
+		}
+	}
+};
