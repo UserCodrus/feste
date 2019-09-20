217c410f184368a8620e20770a6c04a8d8288639
UserCodrus <powerinblack@gmail.com>
2019-09-20 13:11:52 -0400

    Refined the sprite editor's appearance

---
+++ src/spriteeditor.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+var SpriteEditor = {
+	begin: function () {
+		SpriteEditor.hideTabs();
+	},
+
+	hideTabs: function () {
+		// Get the selected type
+		let type = document.forms["sprites"]["type"].value;
+
+		// Hide unneeded tabs
+		let tab = document.getElementById("animation_tab");
+		if (type == "sheet") {
+			tab.style.display = "block";
+		}
+		else {
+			tab.style.display = "none";
+		}
+
+		tab = document.getElementById("background_tab");
+		if (type == "background") {
+			tab.style.display = "table-row";
+		}
+		else {
+			tab.style.display = "none";
+		}
+
+		tab = document.getElementById("sheet_width_tab");
+		if (type == "sheet") {
+			tab.style.display = "table-row";
+		}
+		else {
+			tab.style.display = "none";
+		}
+
+		tab = document.getElementById("sheet_height_tab");
+		if (type == "sheet") {
+			tab.style.display = "table-row";
+		}
+		else {
+			tab.style.display = "none";
+		}
+	}
+}
